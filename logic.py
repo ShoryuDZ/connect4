db9b134b2a5bd5079e2e0af07d1d6a15d47bd281
Shoryu Das-Zaman <58425525+ShoryuDZ@users.noreply.github.com>
2020-04-21 00:43:55 +1000

    Computer Player #7 + Input Validation
    Computer Player #7 complete, with a random moving CPU Player.
    
    Also managing Value Errors in askValue().

--- logic.py
+++ logic.py
@@ -1,5 +1,6 @@
 from operator import itemgetter
 import math
+import random
 
 board = []
 indicators = []
@@ -7,9 +8,15 @@ filledCells = []
 moveNumber = 0
 playerNumber = 1
 connect = 0
+cpuPlayer = False
 
 def initialiser():
     global connect
+    global cpuPlayer
+    
+    cpuPlayerInt = askValue("Number of players - 1 (vs CPU) or 2 (1 vs 1): ", "Please enter a number between 1 and 2", 1, 2)
+    cpuPlayer = cpuPlayerInt == 1
+    
     connect = askValue("How many tokens must be connected? ", "Please allow for at least 3 tokens to be connected", 3, math.inf)
     numberOfRows = askValue("Number of rows: ", "Minimum height of 4", 4, math.inf)
     numberOfColumns = askValue("Number of columns: ", "Minimum width of 4", 4, math.inf)
@@ -31,11 +38,15 @@ def initialiser():
         y += 1
 
 def askValue(question, errorMessage, minimum, maximum):
-    value = int(input(question))
-    while value < minimum or value > maximum:
-        print(errorMessage)
+    try:
         value = int(input(question))
-    return value
+        while value < minimum or value > maximum:
+            print(errorMessage)
+            value = int(input(question))
+        return value
+    except (ValueError):
+        print("Please enter an integer")
+        return askValue(question, errorMessage, minimum, maximum)
     
 def printBoard():
     for row in board:
@@ -48,7 +59,12 @@ def insertToken():
     global moveNumber
     global playerNumber
     playerNumber = (moveNumber % 2) + 1
-    insertionPosition = askValue("Player " + str(playerNumber) + "'s column to place token: ", "Please enter a valid column (1 - " + str(len(board[0])) + ").", 1, len(board[0])) - 1
+
+    if (cpuPlayer and playerNumber == 2):
+        print("Computer making move")
+        insertionPosition = cpuInsertToken()
+    else:
+        insertionPosition = askValue("Player " + str(playerNumber) + "'s column to place token: ", "Please enter a valid column (1 - " + str(len(board[0])) + ").", 1, len(board[0])) - 1
     
     inserted = False
     y = len(board) - 1
@@ -63,7 +79,18 @@ def insertToken():
         moveNumber += 1
         printBoard()
     else:
-        print("Column Full! Please reselect.")
+        if not (cpuPlayer and playerNumber == 2):
+            print("Column Full! Please reselect.")
+
+def cpuInsertToken():
+    availableColumns = []
+    x = 0
+    while x < len(board[0]):
+        if board[0][x] == 0:
+            availableColumns.append(x)
+        x += 1
+    pickedColumn = availableColumns[random.randint(0, len(availableColumns) - 1)]
+    return pickedColumn
 
 def fullBoardChecker():
     full = False
@@ -110,4 +137,10 @@ printBoard()
 while continueGame():
     insertToken()
 
-print("Game Over ~Player " + str(playerNumber) + " wins!~")
+gameOverString =  "Game Over ~"
+if cpuPlayer and playerNumber == 2:
+    gameOverString += "CPU Wins!~"
+else:
+    gameOverString += "Player " + str(playerNumber) + " wins!~"
+
+print(gameOverString)
