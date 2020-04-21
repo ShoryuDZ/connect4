73561e5fe4966c1341109df3934cf38f987552d9
Shoryu Das-Zaman <58425525+ShoryuDZ@users.noreply.github.com>
2020-04-21 22:16:27 +1000

    Moving to Classes
    Moved all code into classes as per #11

--- logic.py
+++ logic.py
@@ -2,145 +2,127 @@ from operator import itemgetter
 import math
 import random
 
-board = []
-indicators = []
-filledCells = []
-moveNumber = 0
-playerNumber = 1
-connect = 0
-cpuPlayer = False
+class Board:
+    board = []
+    indicators = []
+    filledCells = []
+    connect = 0
 
-def initialiser():
-    global connect
-    global cpuPlayer
-    
-    cpuPlayerInt = askValue("Number of players - 1 (vs CPU) or 2 (1 vs 1): ", "Please enter a number between 1 and 2", 1, 2)
-    cpuPlayer = cpuPlayerInt == 1
-    
-    connect = askValue("How many tokens must be connected? ", "Please allow for at least 3 tokens to be connected", 3, math.inf)
-    numberOfRows = askValue("Number of rows: ", "Minimum height of 4", 4, math.inf)
-    numberOfColumns = askValue("Number of columns: ", "Minimum width of 4", 4, math.inf)
-
-    x = 0
-    y = 0
-
-    while x < numberOfRows:
-        row = []
-        while y < numberOfColumns:
-            row.append(0)
-            y += 1
-        board.append(row)
-        x += 1
+    def __init__(self, height, length, connect):
+        self.connect = connect
+        
+        x = 0
         y = 0
 
-    while y < numberOfColumns:
-        indicators.append(y + 1)
-        y += 1
+        while x < height:
+            row = []
+            while y < length:
+                row.append(0)
+                y += 1
+            self.board.append(row)
+            x += 1
+            y = 0
 
-def askValue(question, errorMessage, minimum, maximum):
-    try:
-        value = int(input(question))
-        while value < minimum or value > maximum:
-            print(errorMessage)
+        while y < length:
+            self.indicators.append(y + 1)
+            y += 1
+        
+    def printBoard(self):
+        for row in self.board:
+            print(" "*16, end='')
+            print(row)
+        print("Column Numbers:", end=' ')
+        print(self.indicators)
+
+    def insertToken(self, insertionPosition, player):
+        inserted = False
+        y = len(self.board) - 1
+        while y >= 0 and not inserted:
+            if self.board[y][insertionPosition] == 0:
+                self.board[y][insertionPosition] = player.number
+                self.filledCells.append([insertionPosition, y, player.number])
+                self.filledCells.sort()
+                inserted = True
+            y -= 1
+        if inserted:
+            self.printBoard()
+        else:
+            if not player.CPU:
+                print("Column Full! Please reselect.")
+    
+    def newMove(self, player):
+        if (player.CPU):
+            print("Computer making move")
+            insertionPosition = self.cpuInsert()
+        else:
+            insertionPosition = Input.askValue("Player " + str(player.number) + "'s column to place token: ", "Please enter a valid column (1 - " + str(len(self.board[0])) + ").", 1, len(self.board[0])) - 1
+        
+        self.insertToken(insertionPosition, player)
+    
+    def cpuInsert(self):
+        availableColumns = []
+        x = 0
+        while x < len(self.board[0]):
+            if self.board[0][x] == 0:
+                availableColumns.append(x)
+            x += 1
+        pickedColumn = availableColumns[random.randint(0, len(availableColumns) - 1)]
+        return pickedColumn
+    
+    def fullBoardChecker(self):
+        full = False
+        x = 0
+        while not full and x < len(self.board[0]):
+            if self.board[0][x] == 0:
+                full = True
+            x += 1
+        return full
+
+    def connectChecker(self):
+        for cell in self.filledCells:
+            horizontalConnect = cell[0] <= len(self.board[0]) - self.connect and self.connectTokens("horizontal", cell)
+            verticalConnect = cell[1] <= len(self.board) - self.connect and self.connectTokens("vertical", cell)
+            upwardsDiagonalConnect = cell[0] <= len(self.board[0]) - self.connect and cell[1] >= self.connect - 1 and self.connectTokens("upwards diagonal", cell)
+            downwardsDiagonalConnect = cell[0] <= len(self.board[0]) - self.connect and cell[1] <= len(self.board) - self.connect and self.connectTokens("downwards diagonal", cell)
+            if horizontalConnect or verticalConnect or upwardsDiagonalConnect or downwardsDiagonalConnect: 
+                return True
+
+    def connectTokens(self, direction, basecell):
+        adjacentCells = []
+        adjacentCells.append(basecell)
+        for cell in self.filledCells:
+            if self.passConditional(direction, cell, basecell):
+                adjacentCells.append(cell)
+                basecell = cell
+        return len(adjacentCells) >= self.connect
+
+    def passConditional(self, direction, cell, basecell):
+        if (direction == "horizontal"):
+            return cell[1] == basecell[1] and cell[0] == basecell[0] + 1 and cell[2] == basecell[2]
+        elif (direction == "vertical"):
+            return cell[0] == basecell[0] and cell[1] == basecell[1] + 1 and cell[2] == basecell[2]
+        elif (direction == "upwards diagonal"):
+            return cell[0] == basecell[0] + 1 and cell[1] == basecell[1] - 1 and cell[2] == basecell[2]
+        elif (direction == "downwards diagonal"):
+            return cell[0] == basecell[0] + 1 and cell[1] == basecell[1] + 1 and cell[2] == basecell[2]
+
+class Player:
+    number = 0
+    CPU = False
+    
+    def __init__(self, number, CPU):
+        self.number = number
+        self.CPU = CPU
+
+class Input:
+    @staticmethod
+    def askValue(question, errorMessage, minimum, maximum):
+        try:
             value = int(input(question))
-        return value
-    except (ValueError):
-        print("Please enter an integer")
-        return askValue(question, errorMessage, minimum, maximum)
-    
-def printBoard():
-    for row in board:
-        print(" "*16, end='')
-        print(row)
-    print("Column Numbers:", end=' ')
-    print(indicators)
-
-def insertToken():
-    global moveNumber
-    global playerNumber
-    playerNumber = (moveNumber % 2) + 1
-
-    if (cpuPlayer and playerNumber == 2):
-        print("Computer making move")
-        insertionPosition = cpuInsertToken()
-    else:
-        insertionPosition = askValue("Player " + str(playerNumber) + "'s column to place token: ", "Please enter a valid column (1 - " + str(len(board[0])) + ").", 1, len(board[0])) - 1
-    
-    inserted = False
-    y = len(board) - 1
-    while y >= 0 and not inserted:
-        if board[y][insertionPosition] == 0:
-            board[y][insertionPosition] = playerNumber
-            filledCells.append([insertionPosition, y, playerNumber])
-            filledCells.sort()
-            inserted = True
-        y -= 1
-    if inserted:
-        moveNumber += 1
-        printBoard()
-    else:
-        if not (cpuPlayer and playerNumber == 2):
-            print("Column Full! Please reselect.")
-
-def cpuInsertToken():
-    availableColumns = []
-    x = 0
-    while x < len(board[0]):
-        if board[0][x] == 0:
-            availableColumns.append(x)
-        x += 1
-    pickedColumn = availableColumns[random.randint(0, len(availableColumns) - 1)]
-    return pickedColumn
-
-def fullBoardChecker():
-    full = False
-    x = 0
-    while not full and x < len(board[0]):
-        if board[0][x] == 0:
-            full = True
-        x += 1
-    return full
-
-def continueGame():
-    return fullBoardChecker() and not connectChecker()
-
-def connectChecker():
-    for cell in filledCells:
-        horizontalConnect = cell[0] <= len(board[0]) - connect and connectTokens("horizontal", cell)
-        verticalConnect = cell[1] <= len(board) - connect and connectTokens("vertical", cell)
-        upwardsDiagonalConnect = cell[0] <= len(board[0]) - connect and cell[1] >= connect - 1 and connectTokens("upwards diagonal", cell)
-        downwardsDiagonalConnect = cell[0] <= len(board[0]) - connect and cell[1] <= len(board) - connect and connectTokens("downwards diagonal", cell)
-        if horizontalConnect or verticalConnect or upwardsDiagonalConnect or downwardsDiagonalConnect: 
-            return True
-
-def connectTokens(direction, basecell):
-    adjacentCells = []
-    adjacentCells.append(basecell)
-    for cell in filledCells:
-        if passConditional(direction, cell, basecell):
-            adjacentCells.append(cell)
-            basecell = cell
-    return len(adjacentCells) >= connect
-
-def passConditional(direction, cell, basecell):
-    if (direction == "horizontal"):
-        return cell[1] == basecell[1] and cell[0] == basecell[0] + 1 and cell[2] == basecell[2]
-    elif (direction == "vertical"):
-        return cell[0] == basecell[0] and cell[1] == basecell[1] + 1 and cell[2] == basecell[2]
-    elif (direction == "upwards diagonal"):
-        return cell[0] == basecell[0] + 1 and cell[1] == basecell[1] - 1 and cell[2] == basecell[2]
-    elif (direction == "downwards diagonal"):
-        return cell[0] == basecell[0] + 1 and cell[1] == basecell[1] + 1 and cell[2] == basecell[2]
-
-initialiser()
-printBoard()
-while continueGame():
-    insertToken()
-
-gameOverString =  "Game Over ~"
-if cpuPlayer and playerNumber == 2:
-    gameOverString += "CPU Wins!~"
-else:
-    gameOverString += "Player " + str(playerNumber) + " wins!~"
-
-print(gameOverString)
+            while value < minimum or value > maximum:
+                print(errorMessage)
+                value = int(input(question))
+            return value
+        except (ValueError):
+            print("Please enter an integer")
+            return askValue(question, errorMessage, minimum, maximum)
--- main.py
+++ main.py
@@ -1,5 +1,49 @@
 from tkinter import *
+from logic import *
+import math
 
+def initialiser():
+    
+    cpuPlayerInt = Input.askValue("Number of players - 1 (vs CPU) or 2 (1 vs 1): ", "Please enter a number between 1 and 2", 1, 2)
+    player1 = Player(1, False)
+    player2 = Player(2, cpuPlayerInt != 2)
+
+    numberOfRows = Input.askValue("Number of rows: ", "Minimum height of 4", 4, math.inf)
+    numberOfColumns = Input.askValue("Number of columns: ", "Minimum width of 4", 4, math.inf)
+    connect = Input.askValue("How many tokens must be connected? ", "Please allow for between 3 and the board width tokens to be connected", 3, numberOfColumns)
+
+    mainBoard = Board(numberOfRows, numberOfColumns, connect)
+
+    return player1, player2, mainBoard
+
+def endGame(winningPlayer):
+    gameOverString =  "Game Over ~"
+    if winningPlayer.CPU:
+        gameOverString += "CPU Wins!~"
+    else:
+        gameOverString += "Player " + str(winningPlayer.number) + " wins!~"
+
+    print(gameOverString)
+
+def continueGame(board, player):
+    if board.fullBoardChecker() and not board.connectChecker():
+        return True
+    endGame(player)
+    return False
+
+if __name__ == "__main__":
+    player1, player2, mainBoard = initialiser()
+    mainBoard.printBoard()
+    lastMover = player2
+    while continueGame(mainBoard, lastMover):
+        if (lastMover == player2):
+            mainBoard.newMove(player1)
+            lastMover = player1
+        else:
+            mainBoard.newMove(player2)
+            lastMover = player2
+
+""" 
 def begin_game():
     print("No func here yet...")
 
@@ -13,4 +57,4 @@ frame.pack()
 slogan = Button(frame, text="Start", command=begin_game)
 slogan.pack(side=LEFT)
 
-window.mainloop()
+window.mainloop() """
